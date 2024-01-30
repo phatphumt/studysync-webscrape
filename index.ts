05ef1a6be792bdc4c8bfff084fbd61f0684f5800
Phatphum Tarsaku <81572783+phatphumt@users.noreply.github.com>
2024-01-30 08:04:21 +0000

    strip individual ulrs

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-import axios from "axios";
 import { load } from "cheerio";
 import { uid } from "uid";
 
-const fetchTitles = async () => {
+const dfkljhkldfh = async () => {
   try {
     const response = await fetch(
       "https://www.google.com/search?q=what+is+anatomy"
@@ -23,5 +22,25 @@ const fetchTitles = async () => {
   }
 };
 
-const stuff = await fetchTitles();
-Bun.write(`./results/results-${uid(4)}.txt`, stuff.join("\n"));
+const stuff = await dfkljhkldfh();
+let actualURL: string[] = []
+stuff.forEach(e => {
+  const a = e.match(/\/url\?q=/g)
+  if (!a) {
+    return
+  }
+  const regx = /^\/url\?q=https:\/\/(.*\.)?google\.com(\/[^&]+)?(&.*)?$/
+  if (regx.test(e)) {
+    return
+  }
+  actualURL.push(e)
+})
+const id = uid(4)
+Bun.write(`./results/urlsss-${id}.txt`, actualURL.join('\n'))
+
+// stuff.forEach(async (e) => {
+//   const data = await fetch(`e)
+//   const datata = await data.text()
+//   const $ = load(datata)
+//   const stuffs = $("body").toArray()
+// })
